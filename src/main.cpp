004a60e5496ec62114cddc752d83f920903aba8b
Kyaha_Yamine <aftai626@outlook.com>
2025-09-30 12:01:25 +0900

    Add wifimanager

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,40 @@
 #include <WiFi.h>
 #include <ArduinoOTA.h>
 #include <ESPmDNS.h>
+#include <WiFiManager.h>
+
+WiFiManager wm;
 
 #define LED_BUILTIN 2
 
 // put function declarations here:
-void connectWiFi(const char* ssid, const char* pass);
-// put function declarations here:
 int myFunction(int, int);
-const char* w_ssid = "kyahayamine";
-const char* w_pass = "kyahayamine";
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   // put your setup code here, to run once:
   Serial.begin(115200);
-  connectWiFi(w_ssid,w_pass);
-  ArduinoOTA.begin();
+  // Wifi接続 繋がらない場合は設定モードに移行
+  if(wm.autoConnect()){
+    Serial.println("connected");
+  }else{
+    Serial.println("failed to connect");
+    ESP.restart();
+  }
   //mdns
   if (MDNS.begin("esp32")) {
     Serial.println("MDNS responder started");
   }
+  
+  ArduinoOTA.begin();
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 	ArduinoOTA.handle(); // OTAのハンドリング
-
 	delay(100);
-
-	if ( WiFi.status() == WL_DISCONNECTED ) {
-		connectWiFi(w_ssid,w_pass); 
-	}
-}
-
-void connectWiFi(const char* ssid, const char* pass){
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
-    
-  }
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("");
-  Serial.println("WiFi connected:"+WiFi.SSID());
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 // put function definitions here:
