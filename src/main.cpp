5665c54027879f2cf5f40ecfa168c896cc96c70a
Kyaha_Yamine <aftai626@outlook.com>
2025-09-30 11:49:56 +0900

    Add OTA function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,57 @@
 #include <Arduino.h>
+#include <EEPROM.h>
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+
+#define LED_BUILTIN 2
 
 // put function declarations here:
+void connectWiFi(const char* ssid, const char* pass);
+// put function declarations here:
 int myFunction(int, int);
+const char* w_ssid = "kyahayamine";
+const char* w_pass = "kyahayamine";
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  connectWiFi(w_ssid,w_pass);
+  ArduinoOTA.begin();
+  //mdns
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+	ArduinoOTA.handle(); // OTAのハンドリング
+
+	delay(100);
+
+	if ( WiFi.status() == WL_DISCONNECTED ) {
+		connectWiFi(w_ssid,w_pass); 
+	}
+}
+
+void connectWiFi(const char* ssid, const char* pass){
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("");
+  Serial.println("WiFi connected:"+WiFi.SSID());
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // put function definitions here:
