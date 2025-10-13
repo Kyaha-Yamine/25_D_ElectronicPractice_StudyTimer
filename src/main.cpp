dd6e63b0800b384ddd6fcf8619daf24dd57af906
Kyaha_Yamine <aftai626@outlook.com>
2025-10-13 10:49:47 +0900

    LED Ticker test

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ WiFiManager wm;
 #define LED_1 2
 
 Ticker ledTicker;
+WiFiServer server(80);
 
 void blinkLed() {
   digitalWrite(LED_1, !digitalRead(LED_1)); // LEDの状態を反転
@@ -43,7 +44,7 @@ void setup() {
     // Wi-Fi接続完了後、点滅を停止してLEDを消灯
     ledTicker.detach();
     digitalWrite(LED_1, LOW);
-    checkForUpdates(); // アップデートをチェック
+    //checkForUpdates(); // アップデートをチェック
   }else{
     Serial.println("failed to connect");
     // タイムアウトまたは失敗後、点滅を停止
@@ -57,12 +58,64 @@ void setup() {
   }
   //OTA
   ArduinoOTA.begin();
+  Serial.println(WiFi.localIP());
+  
+  server.begin();
 }
-
+int value = 0;
 void loop() {
   // put your main code here, to run repeatedly:
 	ArduinoOTA.handle(); // OTAのハンドリング
-	delay(100);
+
+  WiFiClient client = server.available();   // listen for incoming clients
+
+  if (client) {                             // if you get a client,
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(2, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(2, LOW);                // GET /L turns the LED off
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+
 }
 
 void checkForUpdates() {
